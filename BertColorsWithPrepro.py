667e88a9720ac267d8e6d955b3c74802023d2cfa
M4RK-1 <vizimark@gmail.com>
2024-03-06 12:45:33 +0100

    work :p

--- BertColorsWithPrepro.py
+++ BertColorsWithPrepro.py
@@ -21,6 +21,43 @@ class CustomDataset(Dataset):
         return len(self.labels)
 
 
+def evaluate_on_subset(trainer, tokenizer, colors, features, labels, num_items):
+    """
+    Evaluate the model on the last 'num_items' features and labels.
+
+    Parameters:
+    - trainer: The trained Trainer object.
+    - tokenizer: The tokenizer used for the model.
+    - colors: A list of unique color names used for converting labels to numeric values.
+    - features: A DataFrame containing the features.
+    - labels: A DataFrame containing the labels.
+    - num_items: Number of items from the end of the dataset to evaluate on.
+
+    Returns:
+    - Accuracy of the model on the specified subset of the dataset.
+    """
+    # Extract the last 'num_items' features and labels
+    subset_features = features.iloc[-num_items:]
+    subset_labels = labels.iloc[-num_items:]
+
+    # Tokenize the subset features
+    subset_encodings = tokenizer(subset_features[0].tolist(), truncation=True, padding='max_length', max_length=512)
+
+    # Convert the subset labels to numeric values
+    subset_labels_numer = subset_labels[0].apply(lambda x: colors.index(x))
+
+    # Create a dataset with the subset tokenized features and numeric labels
+    subset_dataset = CustomDataset(subset_encodings, subset_labels_numer.tolist())
+
+    # Predict using the trained model
+    predictions = trainer.predict(subset_dataset).predictions
+
+    # Compute accuracy
+    accuracy = compute_accuracy(predictions, subset_labels_numer.tolist())
+
+    print(f"Accuracy on the last 100 features: {accuracy:.4f}")
+
+
 def compute_accuracy(predictions, labels):
     predicted_labels = predictions.argmax(axis=1)
     correct_predictions = (predicted_labels == labels).sum()
@@ -34,26 +71,26 @@ cut_labels = pd.read_csv("Data/ColorsCSV/cut_labels.csv", header=None)
 unique_labels = pd.read_csv("Data/ColorsCSV/unique_labels.csv", header=None)
 use_features = all_features
 
+# load the tokenizer
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
 # get the number of available labels
 num_known_labels = cut_labels.shape[0]
 
 # get the cut features
-cut_features = use_features.iloc[:num_known_labels]
-use_features.drop(cut_features.index, inplace=True)
+use_features, cut_features = use_features[num_known_labels:], use_features[:num_known_labels]
 
 # Convert color labels to numeric values
-colors = unique_labels.iloc[:, 0].unique().tolist()
+uniqe_labels_num = unique_labels.iloc[:, 0].unique().tolist()
 
-cut_labels_numer = cut_labels.iloc[:, 0].apply(lambda x: colors.index(x))
+# convert the cut labels to numeric values
+cut_labels_numer = cut_labels.iloc[:, 0].apply(lambda x: uniqe_labels_num.index(x))
 
 # combine the cut features and labels
 cut_data = pd.concat([cut_features, cut_labels_numer], axis=1)
 
 # split the features into training and evaluation sets
 train_data, eval_data = train_test_split(cut_data, test_size=0.2)
-
-# load the tokenizer
-tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
 # tokenize the datasets
 train_encodings = tokenizer(train_data.iloc[:, 0].tolist(), truncation=True, padding='max_length', max_length=512)
@@ -68,7 +105,7 @@ eval_dataset = CustomDataset(eval_encodings, eval_labels)
 
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
-model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=len(colors))
+model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=len(uniqe_labels_num))
 
 training_args = TrainingArguments(
     output_dir="./results",
@@ -89,48 +126,59 @@ trainer = Trainer(
 )
 trainer.train()
 
-train_predictions = trainer.predict(train_dataset).predictions
-train_accuracy = compute_accuracy(train_predictions, train_labels)
-print(f"Accuracy on training dataset: {train_accuracy:.4f}")
-
-# Use the trained model to predict the color of the remaining features
-additional_features = use_features.iloc[:100]
-use_features.drop(additional_features.index, inplace=True)
-
-# Tokenize the additional features
-additional_encodings = tokenizer(additional_features.iloc[:, 0].tolist(), truncation=True, padding='max_length',
-                                 max_length=512)
-
-# Create a dataset for the additional features
-additional_dataset = CustomDataset(additional_encodings, [0] * len(additional_features))
-
-# Get predictions for the additional dataset
-additional_predictions = trainer.predict(additional_dataset).predictions
-
-# Map predicted indices to color labels
-predicted_labels = [colors[prediction] for prediction in additional_predictions.argmax(axis=1)]
-predicted_labels = pd.Series(predicted_labels, name="color")
-predicted_labels = predicted_labels.apply(colors.index).tolist()
-
-additional_dataset = CustomDataset(additional_encodings, predicted_labels)
-
 trainer.save_model("fine_tuned_bert")
 model = AutoModelForSequenceClassification.from_pretrained("fine_tuned_bert")
 
-# Retrain the model with the additional dataset
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=additional_dataset,
-    eval_dataset=train_dataset,
-    data_collator=data_collator,
-    tokenizer=tokenizer,
-)
+evaluate_on_subset(trainer, tokenizer, uniqe_labels_num, all_features, all_labels, 100)
 
-trainer.train()
+batch_size = 100
 
-# Evaluate the model's performance
-# Accuracy on training dataset
+while len(use_features) > 0:
+    # Determine the size of the next batch
+    current_batch_size = min(batch_size, len(use_features))
+
+    # Use the trained model to predict the color of the remaining features
+    use_features, additional_features = use_features[current_batch_size:], use_features[:current_batch_size]
+
+    # Tokenize the additional features
+    additional_encodings = tokenizer(additional_features.iloc[:, 0].tolist(), truncation=True, padding='max_length',
+                                     max_length=512)
+
+    # Create a dataset for the additional features
+    additional_dataset = CustomDataset(additional_encodings, [0] * len(additional_features))
+
+    # Get predictions for the additional dataset
+    additional_predictions = trainer.predict(additional_dataset).predictions
+
+    # Map predicted indices to color labels
+    predicted_labels = [uniqe_labels_num[prediction] for prediction in additional_predictions.argmax(axis=1)]
+    predicted_labels = pd.Series(predicted_labels, name="color")
+    predicted_labels = predicted_labels.apply(uniqe_labels_num.index).tolist()
+
+    print(str(len(additional_encodings['input_ids'])) + " " + str(len(
+        predicted_labels)))
+
+    additional_dataset = CustomDataset(additional_encodings, predicted_labels)
+
+    # Retrain the model with the additional dataset
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=additional_dataset,
+        eval_dataset=train_dataset,
+        data_collator=data_collator,
+        tokenizer=tokenizer,
+    )
+
+    trainer.train()
+
+    evaluate_on_subset(trainer, tokenizer, uniqe_labels_num, all_features, all_labels, 100)
+
+    trainer.save_model("fine_tuned_bert")
+    model = AutoModelForSequenceClassification.from_pretrained("fine_tuned_bert")
+
+"""
 train_predictions = trainer.predict(train_dataset).predictions
 train_accuracy = compute_accuracy(train_predictions, train_labels)
 print(f"Accuracy on training dataset: {train_accuracy:.4f}")
+"""
--- Window.py
+++ Window.py
@@ -1,23 +1,79 @@
 import sys
+import threading
+import time
+
+from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QApplication, QPushButton, QStackedWidget, QLabel, QFileDialog, QTextEdit, QComboBox, \
-    QScrollArea, QWidget, QVBoxLayout, QHBoxLayout
+    QScrollArea, QWidget, QVBoxLayout, QHBoxLayout, QProgressBar
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer, \
+    DataCollatorWithPadding, TrainerCallback
+
+
+class ProgressCallback(TrainerCallback):
+    def __init__(self, progress_bar, num_update_steps, update_training_status_signal):
+        super().__init__()
+        self.progress_bar = progress_bar
+        self.num_update_steps = num_update_steps
+        self.current_step = 0
+        self.update_training_status_signal = update_training_status_signal
+
+    def on_train_begin(self, args, state, control, **kwargs):
+        self.update_training_status_signal.emit("Training in progress...")
+    def on_step_end(self, args, state, control, **kwargs):
+        self.current_step += 1
+        progress = int((self.current_step / self.num_update_steps) * 100)
+        progress = min(progress, 100)
+        self.progress_bar.setValue(progress)
+
+    def on_train_end(self, args, state, control, **kwargs):
+        self.update_training_status_signal.emit("HIDE_LABEL")
+        self.progress_bar.setValue(100)
+
+
+class CustomDataset(Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: val[idx] for key, val in self.encodings.items()}
+        item['labels'] = self.labels[idx]
+        return item
+
+    def __len__(self):
+        return len(self.labels)
 
 
 class MyApp(QWidget):
+    prediction_ready = pyqtSignal(list, pd.DataFrame, QWidget)
+    update_training_status = pyqtSignal(str)
+
     def __init__(self):
         super().__init__()
-        self.combo_boxes = None
+        self.import_button1 = None
+        self.imported_files_label = None
+        self.combo_boxes = []
         self.stacked_widget = None
         self.dataframes = [None, None, None]
         self.df_possible_labels = None
+        self.progress_bar = None
+        self.training_status_label = QLabel()
         self.initUI()
+        self.update_training_status.connect(self.updateTrainingStatusLabel)
+
+    def updateTrainingStatusLabel(self, text):
+        if text == "HIDE_LABEL":
+            self.training_status_label.hide()
+        else:
+            self.training_status_label.setText(text)
+            self.training_status_label.show()
 
     def initUI(self):
         self.setWindowTitle("SmartLabeler")
-        self.setGeometry(200, 200, 400, 300)
+        self.setGeometry(200, 200, 1000, 700)
 
         layout = QVBoxLayout()
         self.stacked_widget = QStackedWidget()
@@ -38,20 +94,20 @@ class MyApp(QWidget):
         layout.addWidget(button3)
         layout.addWidget(self.stacked_widget)
 
-        import_button1 = QPushButton("Import File 1")
-        import_button2 = QPushButton("Import File 2")
-        import_button3 = QPushButton("Import File 3")
+        self.import_button1 = QPushButton("Load Data File")
+        self.import_button2 = QPushButton("Load Known Labels File")
+        self.import_button3 = QPushButton("Load Unique Labels File")
 
-        import_button1.clicked.connect(self.importData)
-        import_button2.clicked.connect(self.importLabels)
-        import_button3.clicked.connect(self.importLabelTypes)
+        self.import_button1.clicked.connect(self.importData)
+        self.import_button2.clicked.connect(self.importLabels)
+        self.import_button3.clicked.connect(self.importLabelTypes)
 
         self.imported_files_label = QLabel()
 
         page1.layout = QVBoxLayout()
-        page1.layout.addWidget(import_button1)
-        page1.layout.addWidget(import_button2)
-        page1.layout.addWidget(import_button3)
+        page1.layout.addWidget(self.import_button1)
+        page1.layout.addWidget(self.import_button2)
+        page1.layout.addWidget(self.import_button3)
         page1.layout.addWidget(self.imported_files_label)
 
         page1.setLayout(page1.layout)
@@ -63,6 +119,11 @@ class MyApp(QWidget):
         page2.layout = QVBoxLayout()
         page2.layout.addWidget(self.text_edit)
         page2.setLayout(page2.layout)
+
+        self.progress_bar = QProgressBar(self)
+
+        layout.addWidget(self.training_status_label)
+        layout.addWidget(self.progress_bar)
 
         self.setLayout(layout)
 
@@ -79,34 +140,122 @@ class MyApp(QWidget):
         self.stacked_widget.setCurrentIndex(2)
         page3 = self.stacked_widget.widget(2)
 
+        def train_model():
+            all_features = self.dataframes[0]
+            cut_labels = self.dataframes[1]
+            unique_labels = self.dataframes[2]
+            use_features = all_features
+
+            # load the tokenizer
+            tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
+            # get the number of available labels
+            num_known_labels = cut_labels.shape[0]
+
+            # get the cut features
+            use_features, cut_features = use_features[num_known_labels:], use_features[:num_known_labels]
+
+            # Convert labels to numeric values
+            unique_labels_num = unique_labels.iloc[:, 0].unique().tolist()
+
+            # convert the cut labels to numeric values
+            cut_labels_numer = cut_labels.iloc[:, 0].apply(lambda x: unique_labels_num.index(x))
+
+            # combine the cut features and labels
+            cut_data = pd.concat([cut_features, cut_labels_numer], axis=1)
+
+            # split the features into training and evaluation sets
+            train_data, eval_data = train_test_split(cut_data, test_size=0.2)
+
+            # tokenize the datasets
+            train_encodings = tokenizer(train_data.iloc[:, 0].tolist(), truncation=True, padding='max_length',
+                                        max_length=512)
+            eval_encodings = tokenizer(eval_data.iloc[:, 0].tolist(), truncation=True, padding='max_length',
+                                       max_length=512)
+
+            # Convert color labels to lists
+            train_labels = train_data.iloc[:, 1].tolist()
+            eval_labels = eval_data.iloc[:, 1].tolist()
+
+            train_dataset = CustomDataset(train_encodings, train_labels)
+            eval_dataset = CustomDataset(eval_encodings, eval_labels)
+
+            data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+            model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased",
+                                                                       num_labels=len(unique_labels_num))
+
+            training_args = TrainingArguments(
+                output_dir="./results",
+                learning_rate=2e-5,
+                per_device_train_batch_size=4,
+                per_device_eval_batch_size=4,
+                num_train_epochs=15,
+                weight_decay=0.01,
+            )
+
+            # Assume num_epochs, train_dataset, and batch_size are defined
+            num_epochs = training_args.num_train_epochs
+            batch_size = training_args.per_device_train_batch_size
+            num_update_steps = (len(train_dataset) // batch_size + 1) * num_epochs
+
+            progress_callback = ProgressCallback(self.progress_bar, num_update_steps, self.update_training_status)
+
+            trainer = Trainer(
+                model=model,
+                args=training_args,
+                train_dataset=train_dataset,
+                eval_dataset=eval_dataset,
+                data_collator=data_collator,
+                tokenizer=tokenizer,
+                callbacks=[progress_callback],
+            )
+
+            trainer.train()
+
+
+            trainer.save_model("fine_tuned_bert")
+            model = AutoModelForSequenceClassification.from_pretrained("fine_tuned_bert")
+
+            # Use the trained model to predict the color of the remaining features
+            use_features, additional_features = use_features[100:], use_features[:100]
+
+            # Tokenize the additional features
+            additional_encodings = tokenizer(additional_features.iloc[:, 0].tolist(), truncation=True,
+                                             padding='max_length',
+                                             max_length=512)
+
+            # Create a dataset for the additional features
+            additional_dataset = CustomDataset(additional_encodings, [0] * len(additional_features))
+
+            # Make predictions on the additional features
+            additional_predictions = trainer.predict(additional_dataset).predictions
+            predicted_labels = [unique_labels_num[prediction] for prediction in
+                                additional_predictions.argmax(axis=1)]
+
+            # Emit the signal after training is finished
+            self.prediction_ready.emit(predicted_labels, use_features, page3)
+
+        # Connect the signal to a slot that updates the UI
+        self.prediction_ready.connect(self.display_predicted_labels)
+
+        # Start training thread
+        train_thread = threading.Thread(target=train_model)
+        train_thread.start()
+
+    def display_predicted_labels(self, predicted_labels, use_features, page3):
         scroll_area = QScrollArea(self)
         scroll_widget = QWidget()
         scroll_layout = QVBoxLayout(scroll_widget)
 
-        df = pd.concat(self.dataframes[:2], axis=1)
-        nan_mask = df.isna().any(axis=1)
-        df_known = df[~nan_mask]
-        df_predict = df[nan_mask]
-
-        X = df_known.iloc[:, :-1]
-        y = df_known.iloc[:, -1]
-
-        x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-        model = DecisionTreeClassifier()
-        model.fit(x_train, y_train)
-
-        predictions = model.predict(df_predict.iloc[:100, :-1])
-
-        self.combo_boxes = []
-
-        for i, prediction in enumerate(predictions):
+        for i, prediction in enumerate(predicted_labels):
             h_layout = QHBoxLayout()
 
-            value_label = QLabel(f"Value {i}: {df_predict.iloc[i, 0]}")
+            value_label = QLabel(f"Value {i}: {use_features.iloc[i, 0]}")
             combo_box = QComboBox()
             combo_box.addItems(self.df_possible_labels.iloc[:, 0])
             combo_box.setCurrentText(str(prediction))
+            combo_box.setMaximumWidth(150)
 
             self.combo_boxes.append(combo_box)
 
@@ -131,42 +280,64 @@ class MyApp(QWidget):
         layout.addWidget(correct_button)
 
     def correctAllPredictions(self):
-        for i, combo_box in enumerate(self.combo_boxes):
+        corrected_labels = []
+
+        for combo_box in self.combo_boxes:
             corrected_label = combo_box.currentText()
+            corrected_labels.append(corrected_label)
+
+        # Step 3: Convert the list to a DataFrame
+        corrected_labels_df = pd.DataFrame(corrected_labels)
+
+        # Optional: Display the DataFrame, save it to a file, or perform further processing
+        print(corrected_labels_df)  # For demonstration; prints the DataFrame to console
+
+        # If you want to save this DataFrame to a CSV file:
+        #corrected_labels_df.to_csv("corrected_labels.csv", index=False)
 
     def importData(self):
-        file_path, _ = QFileDialog.getOpenFileName(self, "Select File 1 (Data)", "", "CSV Files (*.csv);;All Files (*)")
+        # uncomment this line to use the file dialog
+        # file_path, _ = QFileDialog.getOpenFileName(self, "Select File 1 (Data)", "",
+        #                                           "CSV Files (*.csv);;All Files (*)")
+
+        file_path = "Data/ColorsCSV/features.csv"
         if file_path:
             try:
                 df = pd.read_csv(file_path, delimiter=';', header=None)
                 df.columns = range(df.shape[1])
                 self.dataframes[0] = df
-                self.imported_files_label.setText(f"Data File: {file_path} (Loaded)")
+                self.import_button1.setText(f"Data File: {file_path.split('/')[-1]} (Loaded) ✔")
             except Exception as e:
                 self.imported_files_label.setText(f"Data File: Error loading ({str(e)})")
 
     def importLabels(self):
-        file_path, _ = QFileDialog.getOpenFileName(self, "Select File 2 (Class Labels)", "",
-                                                   "CSV Files (*.csv);;All Files (*)")
+        # uncomment this line to use the file dialog
+        # file_path, _ = QFileDialog.getOpenFileName(self, "Select File 2 (Class Labels)", "",
+        #                                           "CSV Files (*.csv);;All Files (*)")
+
+        file_path = "Data/ColorsCSV/cut_labels.csv"
         if file_path:
             try:
                 df = pd.read_csv(file_path, delimiter=';', header=None)
                 df.columns = range(df.shape[1])
                 self.dataframes[1] = df
-                self.imported_files_label.setText(f"Class Labels File: {file_path} (Loaded)")
+                self.import_button2.setText(f"Class Labels File: {file_path.split('/')[-1]} (Loaded) ✔")
             except Exception as e:
                 self.imported_files_label.setText(f"Class Labels File: Error loading ({str(e)})")
 
     def importLabelTypes(self):
-        file_path, _ = QFileDialog.getOpenFileName(self, "Select File 3 (Possible Labels)", "",
-                                                   "CSV Files (*.csv);;All Files (*)")
+        # uncomment this line to use the file dialog
+        # file_path, _ = QFileDialog.getOpenFileName(self, "Select File 3 (Possible Labels)", "",
+        #                                           "CSV Files (*.csv);;All Files (*)")
+
+        file_path = "Data/ColorsCSV/unique_labels.csv"
         if file_path:
             try:
                 df = pd.read_csv(file_path, delimiter=';', header=None)
                 df.columns = range(df.shape[1])
                 self.dataframes[2] = df
                 self.df_possible_labels = df
-                self.imported_files_label.setText(f"Possible Labels File: {file_path} (Loaded)")
+                self.import_button3.setText(f"Possible Labels File: {file_path.split('/')[-1]} (Loaded) ✔")
             except Exception as e:
                 self.imported_files_label.setText(f"Possible Labels File: Error loading ({str(e)})")
 
